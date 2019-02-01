df962f910e4e5e441da804da3ee18a553913cd2f
Jason Liang <zjasonliang@gmail.com>
2019-02-01 01:45:27 -0800

    Add alignment and padding.

--- dgemm-blocked-final.c
+++ dgemm-blocked-final.c
@@ -30,7 +30,7 @@ lv3 cache: 10k+k
 //128 - 500
 #define REGA 3
 #define REGB 4 // B = 4*4
-#define BLOCK_SIZE1 96
+#define BLOCK_SIZE1 32
 #define BLOCK_SIZE2 96
 
 #endif
@@ -73,24 +73,32 @@ static void do_block1_16(int lda, int M, int N, int K, double* restrict A, doubl
 
 
 
-
+// M = BLOCK_SIZE_M
+// N = BLOCK_SIZE_N
+// K = BLOCK_SIZE_K
 static inline void block_square_multilv0(int lda, int M, int N, int K, double* restrict A, double* restrict B, double* restrict C) {
     for (int i = 0; i < M; i += REGA)
-        for(int j = 0; j < N; j += REGB*4)
+        for(int j = 0; j < N; j += REGB * 4)
             for(int k = 0; k < K; k += BLOCK_SIZE1){
-                int curM = min (REGA, M-i);
-                int curN = min (REGB * 4, N-j);
-                int curK = min (BLOCK_SIZE1, K-k);
+                int curM = REGA;
+                int curN = REGB * 4;
+                int curK = BLOCK_SIZE1;
+
+                avx_kernel(curM, curN, curK, A + i * K + k, B + k * N + j, C + i * N + j);
+
+//                int curM = min (REGA, M-i);
+//                int curN = min (REGB * 4, N-j);
+//                int curK = min (BLOCK_SIZE1, K-k);
                 // int realloada = min(3, lda-N);
                 // int realloadb = min(16, lda-M);
-                if(curM == 3 && curN == REGB * 4)
+//                if(curM == 3 && curN == REGB * 4)
                     do_block3_16(lda, curM, curN, curK, A + i*lda + k, B + k*lda + j, C + i*lda + j);
-                else if(curN == 2 && curN == REGB * 4)
-                    do_block2_16(lda, curM, curN, curK, A + i*lda + k, B + k*lda + j, C + i*lda + j);
-                else if(curN == 1 && curN == REGB * 4)
-                    do_block1_16(lda, curM, curN, curK, A + i*lda + k, B + k*lda + j, C + i*lda + j);
-                else// not enough B
-                    do_block(lda, curM, curN, curK, A + i*lda + k, B + k*lda + j, C + i*lda + j);
+//                else if(curM == 2 && curN == REGB * 4)
+//                    do_block2_16(lda, curM, curN, curK, A + i*lda + k, B + k*lda + j, C + i*lda + j);
+//                else if(curM == 1 && curN == REGB * 4)
+//                    do_block1_16(lda, curM, curN, curK, A + i*lda + k, B + k*lda + j, C + i*lda + j);
+//                else// not enough B
+//                    do_block(lda, curM, curN, curK, A + i*lda + k, B + k*lda + j, C + i*lda + j);
 
             }
 }
@@ -101,15 +109,51 @@ static inline void block_square_multilv0(int lda, int M, int N, int K, double* r
 
 static inline void block_square_multilv1(int lda, int M, int N, int K, double* restrict A, double* restrict B, double* restrict C){ // 3*16
     for (int i = 0; i < M; i += BLOCK_SIZE_M)
-        for(int j = 0; j < N; j += BLOCK_SIZE_N)
-            for(int k = 0; k < K; k += BLOCK_SIZE_K){
-                int curM = min (BLOCK_SIZE_M, M-i);
-                int curN = min (BLOCK_SIZE_N, N-j);
-                int curK = min (BLOCK_SIZE_K, K-k);
+        for(int j = 0; j < N; j += BLOCK_SIZE_N) {
 
-                block_square_multilv0(lda, curM, curN, curK, A + i*lda + k, B + k*lda + j, C + i*lda + j);
+            int curM = min (BLOCK_SIZE_M, M - i);
+            int curN = min (BLOCK_SIZE_N, N - j);
+
+            double __attribute__(( aligned(__BIGGEST_ALIGNMENT__))) C_padded[BLOCK_SIZE_M * BLOCK_SIZE_N];
+            memset(C_padded, 0, sizeof(double) * BLOCK_SIZE_M * BLOCK_SIZE_N);
+            for (int ii = 0; ii < curM; ++ii)
+                memcpy(C_padded + ii * BLOCK_SIZE_N, C + ii * lda, sizeof(double) * curN);
+
+            // double __attribute__((align (32))) A_aligned[lda * lda];
+            // double __attribute__((align (32))) B_aligned[lda * lda];
+            // double __attribute__((align (__BIGGEST_ALIGNMENT__))) C_aligned[lda * lda];
+
+            //    double* restrict A_aligned;
+            //    double* restrict B_aligned;
+
+            //    posix_memalign(&A_aligned, 32, lda * lda * 8);
+            //    posix_memalign(&B_aligned, 32, lda * lda * 8);
+
+            //    memcpy(A_aligned, A, lda * lda * 8);
+            //    memcpy(B_aligned, B, lda * lda * 8);
+            // memcpy(C_aligned, C, lda * lda * 8);
+
+            for (int k = 0; k < K; k += BLOCK_SIZE_K) {
+                int curK = min (BLOCK_SIZE_K, K - k);
+
+                double __attribute__(( aligned(__BIGGEST_ALIGNMENT__))) A_padded[BLOCK_SIZE_M * BLOCK_SIZE_K];
+                double __attribute__(( aligned(__BIGGEST_ALIGNMENT__))) B_padded[BLOCK_SIZE_K * BLOCK_SIZE_N];
+                memset(A_padded, 0, sizeof(double) * BLOCK_SIZE_M * BLOCK_SIZE_K);
+                memset(B_padded, 0, sizeof(double) * BLOCK_SIZE_K * BLOCK_SIZE_N);
+
+                for (int ii = 0; ii < curM; ++ii)
+                    memcpy(A_padded + ii * BLOCK_SIZE_K, A + ii * lda, sizeof(double) * curK);
+
+                for (int kk = 0; kk < curK; ++kk)
+                    memcpy(B_padded + kk * BLOCK_SIZE_N, B + kk * lda, sizeof(double) * curN);
+
+                block_square_multilv0(lda, BLOCK_SIZE_M, BLOCK_SIZE_N, BLOCK_SIZE_K, A_padded, B_padded, C_padded);
 
             }
+
+            for (int ii = 0; ii < curM; ++ii)
+                memcpy(C + ii * lda, C_padded + ii * BLOCK_SIZE_N, sizeof(double) * curN);
+        }
 }
 
 // 96 x 96
@@ -134,23 +178,123 @@ void square_dgemm (int lda, double* restrict A, double* restrict B, double* rest
     // double __attribute__((align (32))) B_aligned[lda * lda];
     // double __attribute__((align (__BIGGEST_ALIGNMENT__))) C_aligned[lda * lda];
 
-    double* restrict A_aligned;
-    double* restrict B_aligned;
+//    double* restrict A_aligned;
+//    double* restrict B_aligned;
 
-    posix_memalign(&A_aligned, 32, lda * lda * 8);
-    posix_memalign(&B_aligned, 32, lda * lda * 8);
+//    posix_memalign(&A_aligned, 32, lda * lda * 8);
+//    posix_memalign(&B_aligned, 32, lda * lda * 8);
 
-    memcpy(A_aligned, A, lda * lda * 8);
-    memcpy(B_aligned, B, lda * lda * 8);
+//    memcpy(A_aligned, A, lda * lda * 8);
+//    memcpy(B_aligned, B, lda * lda * 8);
     // memcpy(C_aligned, C, lda * lda * 8);
 
     block_square_multilv2(lda, lda, lda, lda, A_aligned, B_aligned, C);
     // memcpy(C, C_aligned, lda * lda * 8);
-    free(A_aligned);
-    free(B_aligned);
+//    free(A_aligned);
+//    free(B_aligned);
 }
 
 
+
+static inline void avx_kernel(int M, int N, int K, double* restrict A, double* restrict B, double* restrict C) {
+    register __m256d c00,c01,c02,c03;
+    register __m256d c10,c11,c12,c13;
+    register __m256d c20,c21,c22,c23;
+    //totally 3*4*4 = 48 8float/per refresh
+    //__m256d zero = _mm256_set1_pd(0.0);
+    c00 = _mm256_loadu_pd(&C[0 * BLOCK_SIZE_N + 0]);
+    c01 = _mm256_loadu_pd(&C[0 * BLOCK_SIZE_N + 4]);
+    c02 = _mm256_loadu_pd(&C[0 * BLOCK_SIZE_N + 8]);
+    c03 = _mm256_loadu_pd(&C[0 * BLOCK_SIZE_N + 12]);
+    c10 = _mm256_loadu_pd(&C[1 * BLOCK_SIZE_N + 0]);
+    c11 = _mm256_loadu_pd(&C[1 * BLOCK_SIZE_N + 4]);
+    c12 = _mm256_loadu_pd(&C[1 * BLOCK_SIZE_N + 8]);
+    c13 = _mm256_loadu_pd(&C[1 * BLOCK_SIZE_N + 12]);
+    c20 = _mm256_loadu_pd(&C[2 * BLOCK_SIZE_N + 0]);
+    c21 = _mm256_loadu_pd(&C[2 * BLOCK_SIZE_N + 4]);
+    c22 = _mm256_loadu_pd(&C[2 * BLOCK_SIZE_N + 8]);
+    c23 = _mm256_loadu_pd(&C[2 * BLOCK_SIZE_N + 12]);
+    for(int p = 0; p < K; p++){
+        // register __m256d b1 = _mm256_loadu_pd(&B[p*lda + bi*8]);//4 8float
+        // register __m256d b2 = _mm256_loadu_pd(&B[p*lda+4+bi*8]);//4 8float
+        //if(ai == 0 && bi == 0){
+        register __m256d a1 = _mm256_broadcast_sd(&A[0 * BLOCK_SIZE_K + p]);
+        //register __m256d a2 = _mm256_broadcast_sd(&A[0*lda+p]);
+        // register __m256d a2 = a1;
+        register __m256d b1 = _mm256_loadu_pd(&B[p * BLOCK_SIZE_N + 0*8]);//4 8float
+        register __m256d b2 = _mm256_loadu_pd(&B[p * BLOCK_SIZE_N + 4 + 0*8]);//4 8float
+        c00 = _mm256_fmadd_pd(a1,b1,c00);
+        c01 = _mm256_fmadd_pd(a1,b2,c01);
+
+        //else if(ai == 0 && bi == 1){
+        // a1 = _mm256_broadcast_sd(&A[0*lda+p]);
+        // a2 = _mm256_broadcast_sd(&A[0*lda+p]);
+        b1 = _mm256_loadu_pd(&B[p * BLOCK_SIZE_N + 1*8]);//4 8float
+        b2 = _mm256_loadu_pd(&B[p * BLOCK_SIZE_N +4+1*8]);//4 8float
+        c02 = _mm256_fmadd_pd(a1,b1,c02);
+        c03 = _mm256_fmadd_pd(a1,b2,c03);
+
+        //else if(ai == 1 && bi == 0){
+        b1 = _mm256_loadu_pd(&B[p * BLOCK_SIZE_N + 0*8]);//4 8float
+        b2 = _mm256_loadu_pd(&B[p * BLOCK_SIZE_N +4+0*8]);//4 8float
+        a1 = _mm256_broadcast_sd(&A[1 * BLOCK_SIZE_K +p]);
+        // a2 = a1;
+        // a2 = _mm256_broadcast_sd(&A[1*lda+p]);
+        c10 = _mm256_fmadd_pd(a1,b1,c10);
+        c11 = _mm256_fmadd_pd(a1,b2,c11);
+
+
+        //else if(ai == 1 && bi == 1){
+        b1 = _mm256_loadu_pd(&B[p * BLOCK_SIZE_N + 1*8]);//4 8float
+        b2 = _mm256_loadu_pd(&B[p * BLOCK_SIZE_N +4+1*8]);//4 8float
+        // a1 = _mm256_broadcast_sd(&A[1*lda+p]);
+        // a2 = _mm256_broadcast_sd(&A[1*lda+p]);
+        c12 = _mm256_fmadd_pd(a1,b1,c12);
+        c13 = _mm256_fmadd_pd(a1,b2,c13);
+
+        //else if(ai == 2 && bi == 0){
+        b1 = _mm256_loadu_pd(&B[p * BLOCK_SIZE_N + 0*8]);//4 8float
+        b2 = _mm256_loadu_pd(&B[p * BLOCK_SIZE_N +4+0*8]);//4 8float
+        a1 = _mm256_broadcast_sd(&A[2*BLOCK_SIZE_K +p]);
+        // a2 = a1;
+        // a2 = _mm256_broadcast_sd(&A[2*lda+p]);
+        c20 = _mm256_fmadd_pd(a1,b1,c20);
+        c21 = _mm256_fmadd_pd(a1,b2,c21);
+
+
+        //else if(ai == 2 && bi == 1){
+        b1 = _mm256_loadu_pd(&B[p * BLOCK_SIZE_N + 1*8]);//4 8float
+        b2 = _mm256_loadu_pd(&B[p * BLOCK_SIZE_N +4+1*8]);//4 8float
+        // a1 = _mm256_broadcast_sd(&A[2*lda+p]);
+        // a2 = _mm256_broadcast_sd(&A[2*lda+p]);
+        c22 = _mm256_fmadd_pd(a1,b1,c22);
+        c23 = _mm256_fmadd_pd(a1,b2,c23);
+
+
+        // if no enough A, then less A is needed;
+        // so we have to consider unsafe M and N, but K is safe
+
+    }
+
+
+    _mm256_storeu_pd(&C[0 * BLOCK_SIZE_N +0*8], c00);
+    _mm256_storeu_pd(&C[0 * BLOCK_SIZE_N +4+0*8], c01);
+
+    _mm256_storeu_pd(&C[0 * BLOCK_SIZE_N +1*8], c02);
+    _mm256_storeu_pd(&C[0 * BLOCK_SIZE_N +4+1*8], c03);
+
+    _mm256_storeu_pd(&C[1 * BLOCK_SIZE_N +0*8], c10);
+    _mm256_storeu_pd(&C[1 * BLOCK_SIZE_N +4+0*8], c11);
+
+    _mm256_storeu_pd(&C[1 * BLOCK_SIZE_N +1*8], c12);
+    _mm256_storeu_pd(&C[1 * BLOCK_SIZE_N +4+1*8], c13);
+
+    _mm256_storeu_pd(&C[2 * BLOCK_SIZE_N +0*8], c20);
+    _mm256_storeu_pd(&C[2 * BLOCK_SIZE_N +4+0*8], c21);
+
+    _mm256_storeu_pd(&C[2 * BLOCK_SIZE_N +1*8], c22);
+    _mm256_storeu_pd(&C[2 * BLOCK_SIZE_N +4+1*8], c23);
+}
 
 
 
